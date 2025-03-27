ab24489e191fbf6b52a28b05d2471ae86c192fa0
Wilson Li <wilsonli@fortinet.com>
2025-03-27 10:00:09 -0700

    sql gen

---
+++ db/generate_sql_all_domains.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/dev1
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/dev2
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/DevQAEurope
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/DevQAGlobal
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/DevQAJapan
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/DevQAUS
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/eu
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/globalnew
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/jp
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/minidr
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/ottawa
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/paris
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/RE
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/RG
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/RJ
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/staging
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/stagingtst
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/us
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/us_burnaby
+python3 generate_sql.py /etc/fcld/serverconf/FortiGateCloud/deployconf/us_plano
